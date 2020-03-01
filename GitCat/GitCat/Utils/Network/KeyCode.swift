03a0cbca4ae2f6ce7f3f95e271b08c5ecfb5ce58
조윤영 <yoon980208@gmail.com>
2020-03-02 03:28:28 +0900

    [TEST] gitignore 반영 확인

---
+++ GitCat/GitCat/Utils/Network/KeyCode.swift
@@ -0,0 +1,19 @@
+//
+//  KeyCode.swift
+//  GitCat
+//
+//  Created by 조윤영 on 28/02/2020.
+//  Copyright © 2020 조윤영. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+enum Secrets {
+    static let PrivatekeyData = ""//반영확인 3
+    static let PrivateivData = ""
+    
+}
+
+
+
