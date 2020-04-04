3e0e1989818015e39e005ebc9c2fc1d642a18ef9
조윤영 <yoon980208@gmail.com>
2020-04-04 14:13:20 +0900

    [UPDATE]세로 모드 고정

--- GitCat/GitCat/AppDelegate.swift
+++ GitCat/GitCat/AppDelegate.swift
@@ -102,6 +102,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
     
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?)
+    -> UIInterfaceOrientationMask {
+            return [.portrait, .portraitUpsideDown]
+    }
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
