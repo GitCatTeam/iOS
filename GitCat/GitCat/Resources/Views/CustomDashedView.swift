e171e4a471efae305adbacce60a3d765c6026c9b
조윤영 <yoon980208@gmail.com>
2020-04-08 13:59:46 +0900

    [UPDATE]리포트 점선 색 변경

--- GitCat/GitCat/Resources/Views/CustomDashedView.swift
+++ GitCat/GitCat/Resources/Views/CustomDashedView.swift
@@ -19,6 +19,7 @@ class CustomDashedView: UIView {
         let dashPattern: [CGFloat] = [5.0, 3.0]
         path.setLineDash(dashPattern, count: dashPattern.count, phase: 0)
         // <<
+        #colorLiteral(red: 0.7529411765, green: 0.7529411765, blue: 0.7529411765, alpha: 1).set()
         path.lineWidth = 1
         path.move(to: CGPoint(x: 0, y: 0))
         path.addLine(to: CGPoint(x: self.bounds.width, y: 0))
